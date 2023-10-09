6bf331ad3bd46ffe962645ec085883d85eb34994
n-kalu <nnabugwuezema@gmail.com>
2023-10-09 03:18:17 -0600

    Added Switch Statement Codes. Created functions to handle verification of user input and printing of results

--- index.c
+++ index.c
@@ -3,6 +3,21 @@
 #include <math.h>
 #include <stdbool.h>
 
+void ClearStdinBuffer();
+void PrintCellNumbers();
+char RegisterPlayedCell(char cNumber, int tPlay);
+void DisplayEmptyCells();
+
+char fst = ' ';
+char snd = ' ';
+char thd = ' ';
+char fth = ' ';
+char fif = ' ';
+char six = ' ';
+char sev = ' ';
+char eit = ' ';
+char nit = ' ';
+
 int main()
 {
     printf("\n");
@@ -19,79 +34,157 @@ int main()
     int turnToPlay = 0;
     int curPlay = 0;
     bool play = true;
+    //Track cell count after each game
+    // int oddCellCount = 0;
+    int evenCellCount = 0;
 
 
     printf("Enter Your Name: ");
     scanf("%s", &name);
 
     //Clear StdIn buffer. 
-    getchar();
-    printf("\nWelcome %s. You will be playing against the Computer today.", name);
-    printf("\n\nTo play, please enter a cell number. See cell numbers below.\n\n\n");
+    ClearStdinBuffer();
 
-    printf("       |       |       \n");
-    printf("   1   |   2   |   3   \n");
-    printf("-------|-------|-------\n");
-    printf("       |       |       \n");
-    printf("   4   |   5   |   6   \n");
-    printf("       |       |       \n");
-    printf("-------|-------|-------\n");
-    printf("   7   |   8   |   9   \n");
-    printf("       |       |       \n");
+    printf("\nWelcome %s. You will be playing against the Computer today.", name);
+    printf("\n\nTo play, please enter a cell number. See cell numbers below.\n\n");
+    printf("NOTE: Your entry shows up as 'X' and the computer's entry shows up as 'O'.\n\n\n");
+
+    PrintCellNumbers();
 
     //Run the following code while the play still wants to play (i.e if they answer 'Y' to the question "Are you ready to play?").
     while (play)
     {
-        printf("\nAre you ready to play? Y/N: ");
+        printf("\nStart New Game? Y/N: ");
         scanf("%c", &ready);
+
         //Clear StdIn buffer.
-        getchar();
+        ClearStdinBuffer();
         lwrReady = tolower(ready);
         
         if (lwrReady == 'y')
         {
+            //Check cell count after each game and refresh page when 
+            if(turnToPlay == (evenCellCount * 9))
+            {
+                printf("\e[1;1H\e[2J");
+                PrintCellNumbers();
+                DisplayEmptyCells();
+            }
+
             //Reprint all the cells everytime a player enters a cell number
             while (curPlay < 9)
             { 
+                //Check cell count after each game to refresh page
+                // if(turnToPlay == (9 * oddCellCount))
+                // {
+                //     //reset turnToPlay
+                //     // turnToPlay = 0;
+                //     //Regex to clear Console
+                //     printf("\e[1;1H\e[2J");
+                //     PrintCellNumbers();
+                // }
+                // if(turnToPlay == (18 * evenCellCount))
+                // {
+                //     printf("\e[1;1H\e[2J");
+                //     PrintCellNumbers();
+                // }
+
                 //Transfer control to the other player after a cell number has been entered. Alternate between players. 
                 if(turnToPlay % 2 == 0)
                 {
                     char cellNumber;
-                    printf("Enter Cell Number: ");
+                    printf("\nEnter Cell Number: ");
                     scanf("%c", &cellNumber);
+
+                    //Clear StdIn buffer.
+                    ClearStdinBuffer();
 
                     char first, second, third, fourth, fifth, sixth, seventh, eight, ninth;
                     switch (cellNumber)
                     {
                     case '1':
                         first = cellNumber;
+
+                        //Regex to clear Console
+                        printf("\e[1;1H\e[2J");
+                        PrintCellNumbers();
+                        RegisterPlayedCell(first, turnToPlay);
+
                         break;
                     case '2':
                         second = cellNumber;
+
+                        //Regex to clear Console
+                        printf("\e[1;1H\e[2J");
+                        PrintCellNumbers();
+                        RegisterPlayedCell(second, turnToPlay);
+
                         break;
                     case '3':
                         third = cellNumber;
+
+                        //Regex to clear Console
+                        printf("\e[1;1H\e[2J");
+                        PrintCellNumbers();
+                        RegisterPlayedCell(third, turnToPlay);
+
                         break;
                     case '4':
                         fourth = cellNumber;
+                        //Regex to clear Console
+                        printf("\e[1;1H\e[2J");
+                        PrintCellNumbers();
+                        RegisterPlayedCell(fourth, turnToPlay);
+
                         break;
                     case '5':
                         fifth = cellNumber;
+
+                        //Regex to clear Console
+                        printf("\e[1;1H\e[2J");
+                        PrintCellNumbers();
+                        RegisterPlayedCell(fifth, turnToPlay);
+
                         break;
                     case '6':
                         sixth = cellNumber;
+
+                        //Regex to clear Console
+                        printf("\e[1;1H\e[2J");
+                        PrintCellNumbers();
+                        RegisterPlayedCell(sixth, turnToPlay);
+
                         break;
                     case '7':
                         seventh = cellNumber;
+
+                        //Regex to clear Console
+                        printf("\e[1;1H\e[2J");
+                        PrintCellNumbers();
+                        RegisterPlayedCell(seventh, turnToPlay);
+
                         break;
                     case '8':
                         eight = cellNumber;
+
+                        //Regex to clear Console
+                        printf("\e[1;1H\e[2J");
+                        PrintCellNumbers();
+                        RegisterPlayedCell(eight, turnToPlay);
+
                         break;
                     case '9':
                         ninth = cellNumber;
+
+                        //Regex to clear Console
+                        printf("\e[1;1H\e[2J");
+                        PrintCellNumbers();
+                        RegisterPlayedCell(ninth, turnToPlay);
+
                         break;
                     
                     default:
+                    printf("\nWrong Input. Please ensure to enter 'Y' or 'N'.");
                         break;
                     }
 
@@ -101,6 +194,12 @@ int main()
                 else
                 {
                     // printf("Computer's Turn To Play\n");
+
+                    //Regex to clear Console
+                    printf("\e[1;1H\e[2J");
+                    PrintCellNumbers();
+                    RegisterPlayedCell(5, turnToPlay);
+
                     turnToPlay++;
                 }
                 curPlay++;
@@ -110,7 +209,7 @@ int main()
         }
         else if(lwrReady == 'n')
         {
-            printf("\nNo worries. Thanks for stopping by. Looking forward to playing with you in the future.");
+            printf("\nNo worries. Looking forward to playing with you in the future.");
             play = false;
         }
         else
@@ -121,43 +220,143 @@ int main()
 
         //Reset Trackers after each play is completed
         curPlay = 0;
+        //increment cellCount
+        // oddCellCount += 2;
+        evenCellCount++;
+        
+        //Reset Variables
+        fst = ' ';
+        snd = ' ';
+        thd = ' ';
+        fth = ' ';
+        fif = ' ';
+        six = ' ';
+        sev = ' ';
+        eit = ' ';
+        nit = ' ';
     }
     
+    return (0);
+    printf("\n");
+}
+
+
+// FUNCTIONS//
+void ClearStdinBuffer()
+{
+    while (getchar() != '\n')
+    {
+        
+    }
     
+}
+
+void PrintCellNumbers()
+{
+    printf("      CELL NUMBERS\n\n");
+    printf("       |       |       \n");
+    printf("   1   |   2   |   3   \n");
+    printf("-------|-------|-------\n");
+    printf("       |       |       \n");
+    printf("   4   |   5   |   6   \n");
+    printf("       |       |       \n");
+    printf("-------|-------|-------\n");
+    printf("   7   |   8   |   9   \n");
+    printf("       |       |       \n");
+}
+
+
+char RegisterPlayedCell(char cNumber, int tPlay)
+{
+
+    if(tPlay % 2 > 0)
+    {
+        fif = 'O';
+
+        printf("\n\n      CURRENT GAME");
+        printf("\n\n       |       |       \n");
+        printf("   %c   |   %c   |   %c  \n", fst, snd, thd);
+        printf("-------|-------|-------\n");
+        printf("       |       |       \n");
+        printf("   %c   |   %c   |   %c  \n", fth, fif, six);
+        printf("       |       |       \n");
+        printf("-------|-------|-------\n");
+        printf("   %c   |   %c   |   %c  \n", sev, eit, nit);
+        printf("       |       |       \n");
+    }
+    if(tPlay % 2 == 0)
+    {
+        //Player's turn to play
+
+        if (cNumber == '1')
+        {
+            fst = 'X';
+        }
+        else if (cNumber == '2')
+        {
+            snd = 'X';
+        }
+        else if (cNumber == '3')
+        {
+            thd = 'X';
+        }
+        else if (cNumber == '4')
+        {
+            fth = 'X';
+        }
+         else if (cNumber == '5')
+        {
+            fif = 'X';
+        }
+         else if (cNumber == '6')
+        {
+            six = 'X';
+        }
+         else if (cNumber == '7')
+        {
+            sev = 'X';
+        }
+         else if (cNumber == '8')
+        {
+            eit = 'X';
+        }
+        else
+        {
+            nit = 'X';
+        }
+        
+        printf("\n\n      CURRENT GAME");
+        printf("\n\n       |       |       \n");
+        printf("   %c   |   %c   |   %c  \n", fst, snd, thd);
+        printf("-------|-------|-------\n");
+        printf("       |       |       \n");
+        printf("   %c   |   %c   |   %c  \n", fth, fif, six);
+        printf("       |       |       \n");
+        printf("-------|-------|-------\n");
+        printf("   %c   |   %c   |   %c  \n", sev, eit, nit);
+        printf("       |       |       \n");
+    }
     
+}
 
-////MAY NEED TO CHANGE THIS.
-
-
-    // printf("\n\n\n       |       |       \n");
-    // printf("   %c   |   %c   |   %c  \n", cross, circle, cross);
-    // printf("-------|-------|-------\n");
-    // printf("       |       |       \n");
-    // printf("   %c   |   %c   |   %c  \n", cross, cross, circle);
-    // printf("       |       |       \n");
-    // printf("-------|-------|-------\n");
-    // printf("   %c   |   %c   |   %c  \n", circle, cross, circle);
-    // printf("       |       |       \n");
-
-
-
-
+void DisplayEmptyCells()
+{
+    printf("\n\n      CURRENT GAME");
+    printf("\n\n       |       |       \n");
+    printf("       |       |       \n");
+    printf("-------|-------|-------\n");
+    printf("       |       |       \n");
+    printf("       |       |       \n");
+    printf("       |       |       \n");
+    printf("-------|-------|-------\n");
+    printf("       |       |       \n");
+    printf("       |       |       \n");
+}
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-    // char circle = 'O';
+ // char circle = 'O';
     // char cross = 'X';
 
     // printf("       |       |       \n");
@@ -169,7 +368,3 @@ int main()
     // printf("-------|-------|-------\n");
     // printf("   O   |   O   |   O   \n");
     // printf("       |       |       \n");
-
-    return (0);
-    printf("\n");
-}
